f0a7f63bf7a93aaef2f80e3d3f33620194b10827
ThanhVinh2712 <you@example.com>
2021-09-14 23:07:28 +0700

    branch repo_branch

--- src/test/java/com/example/lenguyenthanhvinhthuchanhtuan1/EmployeeRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.lenguyenthanhvinhthuchanhtuan1;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class EmployeeRepositoryTest {
-}
--- src/test/java/com/example/lenguyenthanhvinhthuchanhtuan1/LeNguyenThanhVinhThucHanhTuan1ApplicationTests.java
+++ src/test/java/com/example/lenguyenthanhvinhthuchanhtuan1/LeNguyenThanhVinhThucHanhTuan1ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.lenguyenthanhvinhthuchanhtuan1;
 
+import com.example.lenguyenthanhvinhthuchanhtuan1.entity.Employee;
+import com.example.lenguyenthanhvinhthuchanhtuan1.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LeNguyenThanhVinhThucHanhTuan1ApplicationTests {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @Test
-    void contextLoads() {
+    void testSaveRepo() {
+        Employee employee = new Employee(1,"Vinh");
+        employeeRepository.save(employee);
     }
 
+    @Test
+    void testReadRepo() {
+        System.out.println(employeeRepository.findAll());
+    }
+
+    @Test
+    void testUpdateRepo() {
+        Employee employee = employeeRepository.findAll().get(0);
+        employee.setName("Thành Vinh");
+        employeeRepository.save(employee);
+    }
+
+    @Test
+    void testDeleteRepo() {
+        employeeRepository.deleteAll();
+    }
 }
