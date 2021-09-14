fdb033f7798a9b083c72b1c97f20aced9a92ef99
ThanhVinh2712 <you@example.com>
2021-09-14 21:38:59 +0700

    add main

---
+++ src/main/java/com/example/lenguyenthanhvinhthuchanhtuan1/LeNguyenThanhVinhThucHanhTuan1Application.java
@@ -0,0 +1,13 @@
+package com.example.lenguyenthanhvinhthuchanhtuan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeNguyenThanhVinhThucHanhTuan1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeNguyenThanhVinhThucHanhTuan1Application.class, args);
+    }
+
+}
