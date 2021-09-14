40bd69d3edb41e666fbcfaf48aca2169896fbf1c
ThanhVinh2712 <you@example.com>
2021-09-14 23:27:52 +0700

    branch jpql_branch

--- src/main/java/com/example/lenguyenthanhvinhthuchanhtuan1/repository/EmployeeRepository.java
+++ src/main/java/com/example/lenguyenthanhvinhthuchanhtuan1/repository/EmployeeRepository.java
@@ -3,6 +3,11 @@ package com.example.lenguyenthanhvinhthuchanhtuan1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.lenguyenthanhvinhthuchanhtuan1.entity.Employee;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+    @Query("from Employee")
+    List<Employee> selectEmployeeJPQL();
 }
--- src/test/java/com/example/lenguyenthanhvinhthuchanhtuan1/LeNguyenThanhVinhThucHanhTuan1ApplicationTests.java
+++ src/test/java/com/example/lenguyenthanhvinhthuchanhtuan1/LeNguyenThanhVinhThucHanhTuan1ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.lenguyenthanhvinhthuchanhtuan1;
 
+import com.example.lenguyenthanhvinhthuchanhtuan1.entity.Employee;
+import com.example.lenguyenthanhvinhthuchanhtuan1.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LeNguyenThanhVinhThucHanhTuan1ApplicationTests {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Test
-    void contextLoads() {
+    void testSelect() {
+        System.out.println(employeeRepository.selectEmployeeJPQL());
     }
 
 }
